6338056989b3757c7a1bba38064a74cb5794717d
ralucaborca <raluca.borca09@gmail.com>
2023-07-29 16:06:56 +0300

    First commit on login!

---
+++ config.js
@@ -0,0 +1,22 @@
+//Configurarea firebase
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
+import 'firebase/compat/auth';
+//import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtH5Szl33Qz1GuvvbyeFpBBUdprlFRXnM",
+  authDomain: "disertatiemphr.firebaseapp.com",
+  projectId: "disertatiemphr",
+  storageBucket: "disertatiemphr.appspot.com",
+  messagingSenderId: "1090616494033",
+  appId: "1:1090616494033:web:da2ab577fd2c63572e82f0",
+  measurementId: "G-J81FNB12YL"
+}
+
+if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
