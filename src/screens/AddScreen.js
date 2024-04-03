7df1490bb0ed9f2d3e2fc8a239050dd84df0d6eb
Raluca Borca <raluca.borca09@gmail.com>
2024-04-03 18:32:16 +0300

    Added retriving date from firebase.

--- src/screens/AddScreen.js
+++ src/screens/AddScreen.js
@@ -1,4 +1,4 @@
-import { View, TextInput, Button, Alert, StyleSheet, TouchableOpacity, Text } from "react-native";
+import { View, TextInput, Button, Alert, StyleSheet, TouchableOpacity, Text, KeyboardAvoidingView } from "react-native";
 import React, {useState, useEffect} from "react";
 import { useNavigation} from '@react-navigation/native';
 import { database, firebase } from '../../config';
@@ -14,10 +14,20 @@ const AddScreen = () => {
     const [afectiune, setAfectiune] = useState('');
     const [fumator, setFumator] = useState('');
     const [practicSport, setPracticSport] = useState('');
+
+    useEffect(() => {
+      const subscriber = auth.onAuthStateChanged(user => {
+        setUser(user); // Set the current user
+      });
+  
+      // Unsubscribe on component unmount
+      return subscriber;
+    }, []); 
   
     const handleSaveData = () => {
-        if (sex && varsta && inaltime && greutate && afectiune && fumator && practicSport) {
+        if (user && sex && varsta && inaltime && greutate && afectiune && fumator && practicSport) {
             database.ref().child('Date pacient').push({
+              user: user.uid,
               sex: sex,
               varsta: varsta,
               inaltime: inaltime,
@@ -45,38 +55,46 @@ const AddScreen = () => {
     };
   
     return (
-      <View style="styles.container">
+      <KeyboardAvoidingView style={styles.container}>
+      <View style={styles.container}>
         <TextInput
+          style={styles.TextInput}
           placeholder="Sex"
           value={sex}
           onChangeText={(text) => setSex(text)}
         />
         <TextInput
+          style={styles.TextInput}
           placeholder="Varsta"
           value={varsta}
           onChangeText={(text) => setVarsta(text)}
         />
         <TextInput
+          style={styles.TextInput}
           placeholder="Inaltime"
           value={inaltime}
           onChangeText={(text) => setInaltime(text)}
         />
         <TextInput
+          style={styles.TextInput}
           placeholder="Greutate"
           value={greutate}
           onChangeText={(text) => setGreutate(text)}
         />
         <TextInput
+          style={styles.TextInput}
           placeholder="Afectiune"
           value={afectiune}
           onChangeText={(text) => setAfectiune(text)}
         />
         <TextInput
+          style={styles.TextInput}
           placeholder="Fumator"
           value={fumator}
           onChangeText={(text) => setFumator(text)}
         />
         <TextInput
+          style={styles.TextInput}
           placeholder="Practic sport"
           value={practicSport}
           onChangeText={(text) => setPracticSport(text)}
@@ -84,16 +102,35 @@ const AddScreen = () => {
 
         <Button title="Save" onPress={handleSaveData} />
       </View>
+      </KeyboardAvoidingView>
     );
   };
 
 export default AddScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems:'center',
-        marginTop:100,
-    }
-
+  container: {
+      flex: 1,
+      alignItems:'center',
+      marginTop:10,
+  },
+  TextInput: {
+      paddingTop:20,
+      paddingBottom:10,
+      width:400,
+      fontSize:20,
+      borderBottomWidth:1,
+      borderBottomColor:'#000',
+      marginBottom:10,
+      textAlign:'center',
+  },
+  button:{
+      marginTop:50,
+      height:70,
+      width:250,
+      backgroundColor:'#A4E8E0',
+      alignItems:'center',
+      justifyContent: 'center',
+      borderRadius:50,
+  }
 })
--- src/screens/HistoryScreen.js
+++ src/screens/HistoryScreen.js
@@ -1,18 +1,56 @@
-import { View, Text, StyleSheet, TouchableOpacity, Footer, FooterTab, Button, Icon } from "react-native";
-import React from "react";
-import { useNavigation} from '@react-navigation/native';
+import { StyleSheet, View, Text, FlatList  } from "react-native";
+import React, {useState, useEffect} from "react";
+import {database} from '../../config';
 
 const HistoryScreen = () => {
-    
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const snapshot = await database.ref().child('Date pacient').once('value');
+          const dataArr = [];
+          snapshot.forEach((childSnapshot) => {
+            dataArr.push({
+              id: childSnapshot.key,
+              ...childSnapshot.val(),
+            });
+          });
+          setData(dataArr);
+          console.log(data);
+        } catch (error) {
+          console.log('Error fetching data: ', error);
+        }
+      };
+  
+      fetchData();
+  
+      return () => {
+        // Cleanup function if needed
+      };
+    }, []);
+    const renderItem = ({ item }) => (
+      <View  style={styles.itemContainer}>
+        <Text>Fumator: {item.fumator}</Text>
+        <Text>Greutate: {item.greutate}</Text>
+        <Text>Afectiune: {item.afectiune}</Text>
+        <Text>Inaltime: {item.inaltime}</Text>
+        <Text>Pacientul practica sport: {item.practicSport}</Text>
+        <Text>Sex: {item.sex}</Text>
+        <Text>Varsta: {item.varsta}</Text>
+        
+      </View>
+      
+    );
     return (
-        <View style={styles.container}>
-            <Text> HistoryScreenScreen</Text>
-            <Text> Inregistrare: {auth.currentUser?.email}</Text>
-
-        </View>
-
-     
-    )
+      <View>
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+    );
 }
 
 export default HistoryScreen;
@@ -21,7 +59,15 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems:'center',
-        marginTop:100,
+        marginTop:20,
+    },
+    itemContainer: {
+      marginBottom: 10, // Adjust this value as needed to add spacing between items
+      padding: 10,
+      backgroundColor: 'white', // Optional background color
+      borderWidth: 1,
+      borderColor: 'gray',
+      borderRadius: 8,
     }
 
 })
