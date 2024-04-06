8bde80246d1e940ba4233168505e98d4ae3e95bd
Raluca Borca <raluca.borca09@gmail.com>
2024-04-06 20:26:45 +0300

    Now the user can see the doctor suggestions. Only their suggestion.

--- src/screens/Screen.js
+++ src/screens/Screen.js
@@ -1,14 +1,59 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import React from "react";
-import { useNavigation} from '@react-navigation/native';
+import { StyleSheet, View, Text, FlatList  } from "react-native";
+import React, {useState, useEffect} from "react";
+import {database, auth } from '../../config';
+
 
 const Screen = () => {
+    const [data, setData] = useState([]);
+    const [userq, setUserQ] = useState(null);
     
-    return (
-        <View style={styles.container}>
-            <Text> Screen</Text>
+    const fetchData = async (user) => {
+        try {
+          const snapshot = await database.ref('Sugestii medic').orderByChild('userId').equalTo(user).once('value');
+          const dataArr = [];
+          console.log(snapshot)
+          snapshot.forEach((childSnapshot) => {
+            dataArr.push({
+              id: childSnapshot.key,
+              ...childSnapshot.val(),
+            });
+          });
+          setData(dataArr);
+        } catch (error) {
+          console.log('Error fetching data: ', error);
+        }
+      };
+  
+      useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+          if (user) {
+            setUserQ(user);
+            fetchData(user.uid);
+          } else {
+            setData([]);
+            setUserQ(null);
+          }
+        });
+    
+        return () => unsubscribe();
+      }, []);
+  
+      const renderItem = ({ item }) => (
+        <View  style={styles.itemContainer}>
+          <Text>Stare de sanatate: {item.stareSanatate}</Text>
+          <Text>Sugestie: {item.sugestie}</Text>
         </View>
-    )
+        
+      );
+      return (
+        <View>
+          <FlatList
+            data={data}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
+      );
 }
 
 export default Screen;
@@ -17,7 +62,15 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems:'center',
-        marginTop:100,
+        marginTop:20,
+    },
+    itemContainer: {
+      marginBottom: 10, // Adjust this value as needed to add spacing between items
+      padding: 10,
+      backgroundColor: 'white', // Optional background color
+      borderWidth: 1,
+      borderColor: 'gray',
+      borderRadius: 8,
     }
 
 })
