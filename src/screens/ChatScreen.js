410457e1538ea5ca13f05abb794991989787da07
Raluca Borca <raluca.borca09@gmail.com>
2024-04-24 23:16:41 +0300

    Added model graph.

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,23 +1,78 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Dimensions } from "react-native";
 import React from "react";
 import { useNavigation} from '@react-navigation/native';
+import { BarChart, Grid, LineChart } from 'react-native-chart-kit';
 
-const ChatScreen = () => {
-    
-    return (
-        <View style={styles.container}>
-            <Text> ChatScreen</Text>
-        </View>
-    )
-}
+  
+  const ChatScreen = () => {
 
-export default ChatScreen;
+      return (
+        <View>
+        <Text style={styles.title}>Graficul pulsului</Text>
+        <LineChart
+          data={{
+            labels: ["January", "February", "March", "April", "May", "June"],
+            datasets: [
+              {
+                data: [
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100
+                ]
+              }
+            ]
+          }}
+          width={Dimensions.get("window").width} // from react-native
+          height={220}
+          yAxisLabel="$"
+          yAxisSuffix="k"
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={{
+            backgroundColor: "#FBE698",
+            backgroundGradientFrom: "#fb8c00",
+            backgroundGradientTo: "#ffa726",
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16
+            },
+            propsForDots: {
+              r: "6",
+              strokeWidth: "2",
+              stroke: "#ffa726"
+            }
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16
+          }}
+        />
+      </View>
+  )};
 
-const styles = StyleSheet.create({
+  export default ChatScreen;
+  
+  const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems:'center',
-        marginTop:100,
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      //backgroundColor: '#F5FCFF',
+    },
+    graph: {
+      marginVertical: 8,
+      borderRadius: 16,
+    },
+    title:{
+        marginTop:50,
+        justifyContent: 'center',
+        alignItems: 'center',
     }
-
-})
+  });
+  
+  
